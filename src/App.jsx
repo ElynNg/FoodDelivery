be8a88bc10c70984894b01e0591c94876b39d00d
Eleanor Nguyen <74583218+ElynNg@users.noreply.github.com>
2024-10-07 23:34:56 +0900

    add log in

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './components/LoginPopup/LoginPopup'
+import { useState } from 'react'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/order' element={<PlaceOrder/>}/>
-      </Routes>
-    <Footer/>
-    </div>
+    <>
+      {showLogin?<LoginPopup setShowLogin={setShowLogin}/> :  <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin}/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/order' element={<PlaceOrder/>}/>
+        </Routes>
+      <Footer/>
+      </div>
+    </>
   )
 }
 
---
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './LoginPopup.css'
+import { assets } from '../../assets/assets'
+
+const LoginPopup = ({setShowLogin}) => {
+
+    const [currentState, setCurrentState] = useState('sign in')
+
+  return (
+    <div className="login-popup" id="login-popup">
+    <form className="login-popup-container">
+        <div className="login-popup-title">
+            <h2>{currentState}</h2>
+            <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+        </div>
+        
+        <div className="login-popup-inputs">
+            {currentState === 'sign in' ? <></> : <input type="text" placeholder="Enter your name" required/>}
+            
+            <input type="email" placeholder="Enter your email" required />
+            <input type="password" placeholder='Enter your password' required />
+        </div>
+
+        <button>{currentState == "sign up" ? "create account" : "sign in"}</button>
+
+        <div className="login-popup-condition">
+            <input type="checkbox" required />
+            <p>By continuing, I agree to  the terms of use & privacy policy</p>
+        </div>
+
+        {currentState === 'sign in' 
+            ? <p>Create a new account ? <span>click here</span></p>
+            : <p>Already have an account? <span>sign in</span></p>
+        }
+        
+        
+    </form>
+    </div>
+  )
+}
+
+export default LoginPopup
