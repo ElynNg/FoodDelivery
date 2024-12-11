dbb26659264421f7911fa02c4f585a975f57d128
Elyn Nguyen <ndym1489@gmail.com>
2024-12-11 23:55:14 +0900

    add logout session to clear process

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -14,6 +14,6 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage})
 
-foodRoute.post("/add", addFood)
+foodRoute.post("/add", upload.single('image'), addFood);
 
 export default foodRoute;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRoute from "./routes/foodRoute.js";
+import session from "express-session";
 
 //app config
 const app = express()
@@ -10,6 +11,26 @@ const port = 9999;
 //middleware
 app.use(express.json())
 app.use(cors())
+
+app.use(
+    session({
+        secret: 'my-practice-project',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {maxAge: 1000*60*60}
+    })
+)
+
+app.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error destroying session:", err);
+            return res.status(500).send('Failed to clear session');
+        }
+
+        res.send('Session cleared successfully');
+    });
+});
 
 //db connect
 connectDB();
